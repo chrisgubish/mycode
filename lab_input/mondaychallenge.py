6b76c1eaf96500d7554335c753cd7ad5536fa1b4
chrisgubish <chris.p.gubish@gmail.com>
2023-07-05 17:33:10 +0000

    join and split

---
+++ lab_input/mondaychallenge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main ():
+
+     user_name =input("What is your name? \n")
+
+     user_day = input("What day of the week is it? \n")
+
+     print("Hello, " + user_name + "! Happy " + user_day +"!")
+
+main()
+
