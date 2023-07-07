98020dd6bebece38fb7997dbaf932bc7169274e7
chrisgubish <chris.p.gubish@gmail.com>
2023-07-07 21:31:23 +0000

    Looping techniques

--- miniproject1.py
+++ miniproject1.py
@@ -8,22 +8,20 @@ user_input = input("Are you having chest pains? \n>")
 if user_input == "Yes":
    user_input2 = print(input("Do you need us to contact emergency personnel for you? \n>"))
 
-   if user_input == "Yes" and "Yes":
+   if user_input2 == "Yes":
         print("Thank you, we are contacting emergency personnel right now")
+   
+   if user_input2 == "No":
+        print("Thank you, we will not contact emergency personnel at this time however we are here to help")
 
-        if user_input == "Yes" and "No":
-            print("Thank you, we will not contact emergency personnel at this time however we are here to help")
-
-        else:
-            print("We are here to help. Please let us know if you need assistance")
 
 elif user_input == "I don't know":
-    print(input("Are you having difficulty breathing? \n>"))
+   user_input3 = print(input("Are you having difficulty breathing? \n>"))
     
-    if user_input == "I don't know" and "yes":
+   if user_input == "I don't know" and user_input3 == "yes":
              print("We can contact emergency personnel")
 
-    elif user_input == "I don't know" and "yes":
+   if user_input == "I don't know" and user_input3 == "no":
             print("Please seek medical help")
 
     
