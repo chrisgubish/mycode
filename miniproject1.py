2444964a74db61333c8b4db9e83e79e41250b64e
chrisgubish <chris.p.gubish@gmail.com>
2023-07-07 20:32:05 +0000

    studying for logic

--- miniproject1.py
+++ miniproject1.py
@@ -8,23 +8,27 @@ user_input = input("Are you having chest pains? \n>")
 if user_input == "Yes":
    user_input2 = print(input("Do you need us to contact emergency personnel for you? \n>"))
 
-if user_input2 == "Yes":
+   if user_input == "Yes" and "Yes":
         print("Thank you, we are contacting emergency personnel right now")
 
-if user_input2 == "No":
-        print("Thank you, we will not contact emergency personnel at this time however we are here to help")
+        if user_input == "Yes" and "No":
+            print("Thank you, we will not contact emergency personnel at this time however we are here to help")
 
-if user_input2 == "I dont know":
-        print("We are here to help. Please let us know if you need assistance")
+        else:
+            print("We are here to help. Please let us know if you need assistance")
 
-else:
-        print("You did not provide input")
+elif user_input == "I don't know":
+    print(input("Are you having difficulty breathing? \n>"))
+    
+    if user_input == "I don't know" and "yes":
+             print("We can contact emergency personnel")
 
+    elif user_input == "I don't know" and "yes":
+            print("Please seek medical help")
 
-if user_input == "I don't know":
-    print("Are you having difficulty breathing?")
+    
 
-if user_input == "No":
+elif user_input == "No":
     print("Thank you for your response.")
 
 else: 
