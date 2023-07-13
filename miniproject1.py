38dc0d2e71aef2f4f7b0b0dd94b93e59068a0da2
chrisgubish <chris.p.gubish@gmail.com>
2023-07-13 16:01:49 +0000

    Learning about classes

--- miniproject1.py
+++ miniproject1.py
@@ -5,46 +5,45 @@
 def main():
 
     round = 0
-    while True: 
-        round = round + 1
+    while True:  
 
         welcome_message = input("Welcome to the Emergency Heart Attack Hotline. Would you like assistance? \n>")
 
-        if welcome_message == "Yes":
+        if welcome_message.lower() == "yes":
             print("Please continue forward")
  
 
             user_input = input("Are you having chest pains? \n>")
 
-            if user_input == "Yes":
+            if user_input.lower() == "yes":
                 print("Do you need us to contact emergency personnel for you?")
                 
                 user_input2 = input(">")
                    
-                if user_input2 == "Yes":
+                if user_input2.lower() == "yes":
                     print("Thank you, we are contacting emergency personnel right now")
                     break
 
-                elif user_input2 == "No":
+                elif user_input2.lower() == "no":
                     print("Thank you, we will not contact emergency personnel at this time however we are here to help")
                     break
 
 
-            elif user_input == "I don't know":
+            elif user_input.lower() == "i don't know":
                 print("Are you having difficulty breathing?")
                 
                 user_input3 = input(">")
                
-                if user_input3 == "Yes":
+                if user_input3.lower() == "yes":
                     print("We can contact emergency personnel")
                     break
 
-                elif user_input3 == "No":
+                elif user_input3.lower() == "no":
                     print("Please seek medical help")
                     break
 
 
-            elif user_input == "No":
+            elif user_input.lower() == "no":
                 print("Thank you for your response.")
                 break
 
@@ -52,7 +51,7 @@ def main():
                 print("You did not provide input.")
 
 
-        elif welcome_message =="No":
+        elif welcome_message.lower() =="no":
             print("Thank you have a nice day!")
             break
            
